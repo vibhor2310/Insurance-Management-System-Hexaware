f338a3dfb9753446ab0c0520ad80f2926a458d62
vibhor2310 <95543808+vibhor2310@users.noreply.github.com>
2024-10-09 14:57:52 +0530

    Update PolicyNotFoundException.java

--- src/exception/PolicyNotFoundException.java
+++ src/exception/PolicyNotFoundException.java
@@ -2,6 +2,6 @@ package exception;
 
 public class PolicyNotFoundException extends Exception {
     public PolicyNotFoundException(String string) {
-        super("Policy not found with ID: "+string);
+        super(string);
     }
 }
